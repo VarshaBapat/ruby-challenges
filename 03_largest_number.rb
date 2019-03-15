bd7978b7103dbf21d7f0b4813a84b1a1598e502a
VarshaBapat <varshavbapat86@yahoo.com.au>
2019-03-15 12:45:47 +1100

    03_largest_number_comments_added

--- 03_largest_number.rb
+++ 03_largest_number.rb
@@ -16,8 +16,8 @@
 # 
 # Examples - what if both numbers are equal or are strings?
 
-def largest_number
-puts "Please enter the first number"
+def largest_number(*2numbers)
+puts "Please enter the 2 numbers"
 x = gets.to_i
 puts "Please enter the second number"
 y = gets.to_i
@@ -31,3 +31,13 @@ end
 end
 
 largest_number
+
+
+#def sample (*test)
+#puts "The number of parameters is #{test.length}"
+#for i in 0...test.length
+#puts "The parameters are #{test[i]}"
+#end
+#end
+#sample "Zara", "6", "F"
+#sample "Mac", "36", "M", "MCA"
