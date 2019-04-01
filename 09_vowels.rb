ba374f622ad1abd98605faa14023daede7a3dc10
VarshaBapat <varshavbapat86@yahoo.com.au>
2019-04-01 12:28:23 +1100

    09_vowels

--- 09_vowels.rb
+++ 09_vowels.rb
@@ -10,6 +10,29 @@
 # Check your solution by running the tests:
 # ruby tests/09_vowels_test.rb
 
-def vowels (string)
-  # Your code here
+def vowels(input_string)
+result_array = Array.new
+input_array = input_string.downcase.split('')
+result_array = input_array.keep_if { |v| v =~ /[aeiou]/ } 
+return result_array
+#result = {}
+#vowels_array = ["a","e","i","o","u"]
+#count_vowels_array = Array.new
+#vowels_array.each_index {|i| count_vowels_array[i] = input_array.count(vowels_array[i])}
+#count_array_length = count_vowels_array.length
+#i = 0
+#while i < count_array_length do
+#  result.store(vowels_array[i],count_vowels_array[i])
+#  i+= 1
+#end
+#return result
 end
+result_array = Array.new
+result_array = vowels("aeiooudfghaeiouoooooooooo")
+puts result_array
+
+#[a e i o u] [1,2,3,4,5]
+
+#a.keep_if { |v| v =~ /[aeiou]/ }  #=> ["a", "e"]
+#my_hash.each { |key, value| puts "k: #{key}, v: #{value}" }
+
